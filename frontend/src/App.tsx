94b8417ec782152de860a89e7ea361fc4eb36f9f
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-03-24 13:21:57 +0000

    add reacte for mobile via expo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,49 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
 function App() {
-  const [userLocation,setUserLocation]=useState<{
-    latitude:number,
-    longitude:number,
+  const [userLocation, setUserLocation] = useState<{
+    latitude: number;
+    longitude: number;
   } | null>(null);
 
-  const getUserLocation = ()=>{
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition((position)=>{
-        const { latitude, longitude } = position.coords;
-        setUserLocation({ latitude, longitude });
-      },(error) => {
-        console.error("Error get user location: ", error);
-      })
-    }else{
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      const watchId = navigator.geolocation.watchPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error watching user location:", error);
+        },
+        {
+          enableHighAccuracy: false, // ou supprime-le
+  timeout: 20000,
+  maximumAge: 1000
+        }
+      );
+
+      // Nettoyage à la désactivation ou changement de page
+      return () => navigator.geolocation.clearWatch(watchId);
+    } else {
       console.log("Geolocation is not supported by this browser");
     }
-  }
+  }, []);
+
   return (
     <>
-      <h1>Geolocation App</h1>
-      <button onClick={getUserLocation}>Get User Location</button>
-
-      {userLocation && (
+      <h1>Live Geolocation Tracker</h1>
+      {userLocation ? (
         <div>
-          <h2>User Location</h2>
+          <h2>📍 Real-time Location</h2>
           <p>Latitude: {userLocation.latitude}</p>
           <p>Longitude: {userLocation.longitude}</p>
         </div>
+      ) : (
+        <p>Waiting for location...</p>
       )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
