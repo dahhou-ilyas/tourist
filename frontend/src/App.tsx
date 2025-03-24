9f37d14d55804676b5a924e5ff4c2bde58ea3324
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-03-24 12:52:31 +0000

    add dockerfile for frontend and ngnix config and docker compose for the global app (front/back)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,35 @@
-function App() {
+import { useState } from "react"
 
+function App() {
+  const [userLocation,setUserLocation]=useState<{
+    latitude:number,
+    longitude:number,
+  } | null>(null);
+
+  const getUserLocation = ()=>{
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position)=>{
+        const { latitude, longitude } = position.coords;
+        setUserLocation({ latitude, longitude });
+      },(error) => {
+        console.error("Error get user location: ", error);
+      })
+    }else{
+      console.log("Geolocation is not supported by this browser");
+    }
+  }
   return (
     <>
-      <p className="p-13">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Geolocation App</h1>
+      <button onClick={getUserLocation}>Get User Location</button>
+
+      {userLocation && (
+        <div>
+          <h2>User Location</h2>
+          <p>Latitude: {userLocation.latitude}</p>
+          <p>Longitude: {userLocation.longitude}</p>
+        </div>
+      )}
     </>
   )
 }
