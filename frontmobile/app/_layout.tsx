18b0901fafd66a97636bb573b3e31565b9f126d7
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-04-21 15:53:25 +0100

    add safe erea

--- frontmobile/app/_layout.tsx
+++ frontmobile/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 import '../global.css';
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function RootLayout() {
-  return <Stack />;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack />
+    </SafeAreaView>
+  );
 }
