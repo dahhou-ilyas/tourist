b370409420513645268b57a65389e4ab5a3c4000
dahhou.ilyas <dahhou.ilyas@gmail.com>
2025-04-25 18:28:17 +0100

    add note

--- frontmobile/app/maps.tsx
+++ frontmobile/app/maps.tsx
@@ -1,5 +1,8 @@
 import MapComponent from "@/component/MapComponent"
-
+//on doit maintenant de ajouté les palce selection coté admine et aussi dans le coté admine on doit le montioné les place déja
+//selectionné a fin de ne pas selection la meme chose
+//aussu coté de frontend mobile on doit traker le user et l'afficher les place dangeruer qui sont dans un distance de 
+// 1 km de distance sahcant que user et le centre de disk de rayon 1km
 
 const MapPage:React.FC = ()=>{
     return <>
